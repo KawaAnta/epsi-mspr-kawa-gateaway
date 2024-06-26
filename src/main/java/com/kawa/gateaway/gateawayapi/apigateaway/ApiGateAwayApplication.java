af167eacce3108396d87dd0cbbccc9ca77467b4d
Alaa Dhieb <alaa.dhieb1@gmail.com>
2024-06-26 11:31:03 +0200

    Chore : Initialisation du projet.

---
+++ src/main/java/com/kawa/gateaway/gateawayapi/apigateaway/ApiGateAwayApplication.java
@@ -0,0 +1,13 @@
+package com.kawa.gateaway.gateawayapi.apigateaway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGateAwayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGateAwayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kawa/gateaway/gateawayapi/apigateaway/ApigateawayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kawa.gateaway.gateawayapi.apigateaway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApigateawayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
