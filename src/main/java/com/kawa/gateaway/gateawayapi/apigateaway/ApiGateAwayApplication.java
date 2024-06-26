b499bfc1c45c59738a1315741d5b6fead0bb59fa
Alaa Dhieb <alaa.dhieb1@gmail.com>
2024-06-26 13:32:09 +0200

    Chore : Mise en place du socle de l'API GateAway

--- src/main/java/com/kawa/gateaway/gateawayapi/apigateaway/ApiGateAwayApplication.java
+++ src/main/java/com/kawa/gateaway/gateawayapi/apigateaway/ApiGateAwayApplication.java
@@ -3,11 +3,21 @@ package com.kawa.gateaway.gateawayapi.apigateaway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Classe principale de l'API GateAway.
+ */
+// CHECKSTYLE:OFF
 @SpringBootApplication
 public class ApiGateAwayApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ApiGateAwayApplication.class, args);
-	}
+    /**
+     * Méthode principale de lancement du service.
+     *
+     * @param args arguments
+     */
+    public static void main(final String[] args) {
+        SpringApplication.run(ApiGateAwayApplication.class, args);
+    }
 
 }
+// CHECKSTYLE:ON
